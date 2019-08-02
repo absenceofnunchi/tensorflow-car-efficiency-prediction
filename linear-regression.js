ac6b15ed1e01628f058aa7ede5a73a1b0b5ae5b1
J <igibliss00@gmail.com>
2019-08-02 16:49:20 -0400

    Created Linear-regression class with lodash

--- linear-regression.js
+++ linear-regression.js
@@ -1,9 +1,43 @@
 const tf = require('@tensorflow/tfjs')
+const _ = require('lodash')
 
 class LinearRegression {
-    constructor() {
-        
+    constructor(features, labels, options) {
+        this.features = features
+        this.labels = labels
+        // assigns a default value to options so that when options is not input, the whole thing 
+        // doesn't return undefined
+        this.options = Object.assign(
+            { learningRate: 0.1, iterations: 1000 }, 
+            options
+        )
+
+        this.m = 0;
+        this.b = 0;
+    }
+
+    gradientDescent() {
+        const currentGuessForMPG = this.features.map(row => {
+            return this.m * row[0] + this.b
+        })
+
+        const bSlope = _.sum(currentGuessForMPG.map((guess, i ) => {
+            return guess - this.labels[i][0]
+        })) * 2 / this.features.length
+
+        const mSlope = _.sum(currentGuessForMPG.map((guess, i) => {
+            return -1 * this.features[i][0] * (this.labels[i][0] - guess)
+        })) * 2 / this.features.length
+
+        this.m = this.m - mSlope * this.options.learningRate
+        this.b = this.b - bSlope * this.options.learningRate 
+    }
+
+    train() {
+        for (let i = 0; i < this.options.iterations; i++) {
+            this.gradientDescent()
+        }
     }
 }
 
-module.export = LinearRegression
+module.exports = LinearRegression
